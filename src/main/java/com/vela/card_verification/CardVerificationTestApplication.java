1f2beed5d530c8fddae8240b122f8425524e8238
frank okafor <frankokafor22@yahoo.com>
2019-12-09 22:33:56 +0100

    started

---
+++ src/main/java/com/vela/card_verification/CardVerificationTestApplication.java
@@ -0,0 +1,13 @@
+package com.vela.card_verification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardVerificationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardVerificationTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frankokafor/card_verification/CardVerificationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankokafor.card_verification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardVerificationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
