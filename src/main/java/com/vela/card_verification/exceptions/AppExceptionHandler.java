bc5e5f9d938eab0a12d617834ea7e1c841faff73
frank okafor <frankokafor22@yahoo.com>
2019-12-09 23:56:02 +0100

    create exception handler class

--- src/main/java/com/vela/card_verification/exceptions/AppExceptionHandler.java
+++ src/main/java/com/vela/card_verification/exceptions/AppExceptionHandler.java
@@ -7,20 +7,30 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.context.request.WebRequest;
 
 import com.vela.card_verification.messages.ErrorMessage;
+import com.vela.card_verification.responses.InfoResponse;
 
 
 @ControllerAdvice
 public class AppExceptionHandler {
 
-	@ExceptionHandler(value = { CardInfoServiceException.class })
+	@ExceptionHandler(value = { CardInfoServiceException.class, InvalidInputException.class })
 	public ResponseEntity<Object> userServiceExceptionHandler(CardInfoServiceException ex, WebRequest request) {
 		ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
 		return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
+	@ExceptionHandler(value = {org.springframework.web.client.HttpStatusCodeException.class})
+    public ResponseEntity<?> statusCodeException(HttpStatusCodeException ex, WebRequest request){
+        InfoResponse response = new InfoResponse();
+        response.setSuccess(false);
+        response.setPayload(null);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+	
 	@ExceptionHandler(value = { Exception.class })
 	public ResponseEntity<Object> otherExceptionHandler(Exception ex, WebRequest request) {
 		ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
---
+++ src/main/java/com/vela/card_verification/exceptions/InvalidInputException.java
@@ -0,0 +1,20 @@
+package com.vela.card_verification.exceptions;
+
+public class InvalidInputException extends RuntimeException {
+
+	private static final long serialVersionUID = -8274909731175076091L;
+	private String message;
+
+	public InvalidInputException(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
