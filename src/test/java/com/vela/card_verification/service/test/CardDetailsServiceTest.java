59136e31009769f8c027235dd5c0d7750343b4e5
frank okafor <frankokafor22@yahoo.com>
2019-12-10 05:09:41 +0100

    added thymeleaf template engine

---
+++ src/test/java/com/vela/card_verification/service/test/CardDetailsServiceTest.java
@@ -0,0 +1,29 @@
+package com.vela.card_verification.service.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CardDetailsServiceTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testGetCardInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCardStats() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAllCards() {
+		fail("Not yet implemented");
+	}
+
+}
